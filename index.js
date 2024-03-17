bed31aa399a42526af041932a3ccb901a906df8a
DogukanAka <aka.dogukan@gmail.com>
2024-03-17 20:40:26 +0300

    Express Js backend bağlantısı gerçekliştirldi. Express Js ile localhost:3000 üzerinde çalışılabilmekte.

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware ayarları
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ana sayfa için route
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Hakkında sayfası için route
+
+
+// İletişim sayfası için route
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Galeri sayfası için route
+app.get('/gallery.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'gallery.html'));
+});
+
+// Tek hizmetler sayfası için route
+app.get('/single-services.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'single-services.html'));
+});
+
+// Tek standart sayfası için route
+app.get('/single-standard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'single-standard.html'));
+});
+
+// Tek video sayfası için route
+app.get('/single-video.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'single-video.html'));
+});
+
+// Başarılı mağazalar sayfası için route
+app.get('/sucess-stores.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'sucess-stores.html'));
+});
+
+// Eğer endpoint belirtilmemişse index.html'i gönder
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+
+
+
+// Diğer sayfalar için route'lar eklenebilir
+
+// Sunucuyu başlatma
+app.listen(port, () => {
+    console.log(`Uygulama ${port} numaralı port üzerinde çalışıyor`);
+});
